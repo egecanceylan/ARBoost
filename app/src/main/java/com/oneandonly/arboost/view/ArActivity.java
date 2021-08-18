dbadebc3e05046878137a8b1525fabbb405ed8c6
Ahmet Selçuk Tunçer <aselcuktuncer@yahoo.com>
2021-08-18 12:26:31 +0300

    screen integrations with db values

--- app/src/main/java/com/oneandonly/arboost/view/ArActivity.java
+++ app/src/main/java/com/oneandonly/arboost/view/ArActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.ar.core.Anchor;
@@ -35,24 +36,121 @@ public class ArActivity extends AppCompatActivity {
 
         // Credit Card Home Screen with db values
         LayoutInflater layoutInflater = getLayoutInflater();
-        View view = layoutInflater.inflate(R.layout.credit_card_home_screen, null);
+        View creditCardHomeScreen = layoutInflater.inflate(R.layout.credit_card_home_screen, null);
 
-        TextView debt = view.findViewById(R.id.credit_card_home_screen_current_debt);
-        TextView currentLimit = view.findViewById(R.id.credit_card_home_screen_card_limit);
-        TextView totalLimit = view.findViewById(R.id.credit_card_home_screen_total_card_limit);
+        TextView creditCardDebt = creditCardHomeScreen.findViewById(R.id.credit_card_home_screen_current_debt);
+        TextView creditCardCurrentLimit = creditCardHomeScreen.findViewById(R.id.credit_card_home_screen_card_limit);
+        TextView creditCardTotalLimit = creditCardHomeScreen.findViewById(R.id.credit_card_home_screen_total_card_limit);
 
-        debt.setText(String.valueOf(cardmodel.getDebt()));
-        currentLimit.setText(String.valueOf(cardmodel.getAccountLimit()));
-        totalLimit.setText(String.valueOf(cardmodel.getFlexibleAccountLimit()));
+        creditCardDebt.setText(String.valueOf(cardmodel.getDebt()));
+        creditCardCurrentLimit.setText(String.valueOf(cardmodel.getAccountLimit()));
+        creditCardTotalLimit.setText(String.valueOf(cardmodel.getFlexibleAccountLimit()));
 
-//        view.setBackground(getResources().getDrawable(R.drawable.green_circle));
+
 
         // Credit Card Debt Payments with db values
+        View creditCardDebtPaymentScreen = layoutInflater.inflate(R.layout.credit_card_debt_payments, null);
+
+        //TextView currentDebt = creditCardDebtPaymentScreen.findViewById(R.id.credit_card_home_screen_current_debt);
+        TextView creditCardTotalDebt = creditCardDebtPaymentScreen.findViewById(R.id.credit_card_debt_payments_screen_total_debt);
+        TextView creditCardLastPaymentDate = creditCardDebtPaymentScreen.findViewById(R.id.credit_card_debt_payments_screen_last_payment_date);
+        TextView creditCardCutoffDate = creditCardDebtPaymentScreen.findViewById(R.id.credit_card_debt_payments_screen_cutoff_date);
+
+        //currentDebt.setText(String.valueOf(cardmodel.getDebt()));
+        creditCardTotalDebt.setText(String.valueOf(cardmodel.getDebt()));
+        creditCardLastPaymentDate.setText(String.valueOf(cardmodel.getPaymentDueDate()));
+        creditCardCutoffDate.setText(String.valueOf(cardmodel.getCutOffDate()));
+
+        //Credit Card Card Details with db values
+        View creditCardCardDetailsScreen = layoutInflater.inflate(R.layout.credit_card_details, null);
+
+        View creditCardContactlessCircle = creditCardCardDetailsScreen.findViewById(R.id.credit_card_contactless_circle);
+        View creditCardEcomCircle = creditCardCardDetailsScreen.findViewById(R.id.credit_card_ecom_circle);
+        View creditCardMailOrderCircle = creditCardCardDetailsScreen.findViewById(R.id.credit_card_mail_order_circle);
+        View creditCardAutomatedCircle = creditCardCardDetailsScreen.findViewById(R.id.credit_card_automated_circle);
+        View creditCardCurrencyCircle = creditCardCardDetailsScreen.findViewById(R.id.credit_card_currency_circle);
+        View creditCardAccountSumCircle = creditCardCardDetailsScreen.findViewById(R.id.credit_card_account_sum_circle);
+        TextView creditCardEmail = creditCardCardDetailsScreen.findViewById(R.id.credit_card_card_details_screen_email);
+        TextView creditCardExpiryDate = creditCardCardDetailsScreen.findViewById(R.id.credit_card_card_details_screen_expiry_date);
+
+
+        if(cardmodel.isContactless() == true) {
+            creditCardContactlessCircle.setBackground(getResources().getDrawable(R.drawable.green_circle));
+        }else{
+            creditCardContactlessCircle.setBackground(getResources().getDrawable(R.drawable.white_circle));
+        }
+        if(cardmodel.isEcom() == true) {
+            creditCardEcomCircle.setBackground(getResources().getDrawable(R.drawable.green_circle));
+        }else{
+            creditCardEcomCircle.setBackground(getResources().getDrawable(R.drawable.white_circle));
+        }
+        if(cardmodel.isMailOrder() == true) {
+            creditCardMailOrderCircle.setBackground(getResources().getDrawable(R.drawable.green_circle));
+        }else{
+            creditCardMailOrderCircle.setBackground(getResources().getDrawable(R.drawable.white_circle));
+        }
+        /*
+        if(cardmodel.isAutomated() == true) {
+            creditCardAutomatedCircle.setBackground(getResources().getDrawable(R.drawable.green_circle));
+        }else{
+            creditCardAutomatedCircle.setBackground(getResources().getDrawable(R.drawable.white_circle));
+        }if(cardmodel.isCurrency() == true) {
+            creditCardCurrencyCircle.setBackground(getResources().getDrawable(R.drawable.green_circle));
+        }else{
+            creditCardCurrencyCircle.setBackground(getResources().getDrawable(R.drawable.white_circle));
+        }if(cardmodel.isAccounSum() == true) {
+            creditCardAccountSumCircle.setBackground(getResources().getDrawable(R.drawable.green_circle));
+        }else{
+            creditCardAccountSumCircle.setBackground(getResources().getDrawable(R.drawable.white_circle));
+        }
+        */
+        creditCardEmail.setText(String.valueOf(cardmodel.geteAccountStatement()));
+        creditCardExpiryDate.setText(String.valueOf(cardmodel.getExpireDate()));
+
+        //Debit Card Home Screen with db values
+        View debitCardHomeScreen = layoutInflater.inflate(R.layout.debit_card_home_screen, null);
+
+        TextView debitCardBalance = debitCardHomeScreen.findViewById(R.id.debit_card_home_screen_balance);
+        TextView debitCardAccountNumber = debitCardHomeScreen.findViewById(R.id.debit_card_home_screen_account_number);
+        TextView debitCardFlexibleAccountLimit = debitCardHomeScreen.findViewById(R.id.debit_card_home_screen_flexible_account_limit);
+
+        debitCardBalance.setText(String.valueOf(cardmodel.getBalance()));
+        debitCardAccountNumber.setText(String.valueOf(cardmodel.getAccountNumber()));
+        debitCardFlexibleAccountLimit.setText(String.valueOf(cardmodel.getFlexibleAccountLimit()));
+
+        //Debit Card Card Details with db values
+        View debitCardCardDetailsScreen = layoutInflater.inflate(R.layout.debit_card_details, null);
+
+        View debitCardContactlessCircle = debitCardCardDetailsScreen.findViewById(R.id.debit_card_card_details_contactless_circle);
+        View debitCardEcomCircle = debitCardCardDetailsScreen.findViewById(R.id.debit_card_card_details_ecom_circle);
+        View debitCardMailOrderCircle = debitCardCardDetailsScreen.findViewById(R.id.debit_card_card_details_account_sum_circle);
+        TextView debitCardEmail = debitCardCardDetailsScreen.findViewById(R.id.debit_card_card_details_screen_email);
+        TextView debitCardExpiryDate = debitCardCardDetailsScreen.findViewById(R.id.debit_card_card_details_expiry_date);
+
+        if(cardmodel.isContactless() == true) {
+            debitCardContactlessCircle.setBackground(getResources().getDrawable(R.drawable.green_circle));
+        }else{
+            debitCardContactlessCircle.setBackground(getResources().getDrawable(R.drawable.white_circle));
+        }
+        if(cardmodel.isEcom() == true) {
+            debitCardEcomCircle.setBackground(getResources().getDrawable(R.drawable.green_circle));
+        }else{
+            debitCardEcomCircle.setBackground(getResources().getDrawable(R.drawable.white_circle));
+        }
+        if(cardmodel.isMailOrder() == true) {
+            debitCardMailOrderCircle.setBackground(getResources().getDrawable(R.drawable.green_circle));
+        }else{
+            debitCardMailOrderCircle.setBackground(getResources().getDrawable(R.drawable.white_circle));
+        }
+        debitCardEmail.setText(String.valueOf(cardmodel.geteAccountStatement()));
+        debitCardExpiryDate.setText(String.valueOf(cardmodel.getExpireDate()));
+
+
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ar_fragment);
 
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
-            placeTextView(hitResult.createAnchor(), view);
+            placeTextView(hitResult.createAnchor(), debitCardCardDetailsScreen);
         });
 
 //        System.out.println(cardmodel.getCardNumber());
